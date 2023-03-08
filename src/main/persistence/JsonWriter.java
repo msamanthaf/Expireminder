fd877cb474177e87294727341452082ce3ea3c20
Flayre <mariesamantha.f@gmail.com>
2023-03-08 14:37:25 -0800

    Added JSONwriter to persistence

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -1,4 +1,21 @@
 package persistence;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+// Represents a writer that writes JSON representation of  to file
 public class JsonWriter {
+    private static final int TAB = 4;
+
+    // EFFECTS: writes string to file destination
+    public boolean write(String destination, Writable classToSave) {
+        try {
+            PrintWriter printer = new PrintWriter(destination);
+            printer.print(classToSave.toJson().toString(TAB));
+            printer.close();
+            return true;
+        } catch (FileNotFoundException e) {
+            return false;
+        }
+    }
 }
