e5daf93111cf1f1f2ea78ecb87e14f89f4ee1456
Flayre <mariesamantha.f@gmail.com>
2023-04-08 14:12:20 -0700

    Added Event and EventLog test classes

---
+++ src/test/model/EventTest.java
@@ -0,0 +1,49 @@
+package model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit tests for the Event class
+ */
+public class EventTest {
+    private Event e;
+    private Date d;
+
+    //NOTE: these tests might fail if time at which line (2) below is executed
+    //is different from time that line (1) is executed.  Lines (1) and (2) must
+    //run in same millisecond for this test to make sense and pass.
+
+    @BeforeEach
+    public void runBefore() {
+        e = new Event("Sensor open at door");   // (1)
+        d = Calendar.getInstance().getTime();   // (2)
+    }
+
+    @Test
+    public void testEvent() {
+        assertEquals("Sensor open at door", e.getDescription());
+        assertEqualDates(d, e.getDate());
+    }
+
+    private static final String DATE_PATTERN = "EEE MMM dd HH:mm:ss zzz yyyy";
+
+    private static void assertEqualDates(Date expected, Date value) {
+        DateFormat formatter = new SimpleDateFormat(DATE_PATTERN);
+        String strExpected = formatter.format(expected);
+        String strValue = formatter.format(value);
+        assertEquals(strExpected, strValue);
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals(d.toString() + "\n" + "Sensor open at door", e.toString());
+    }
+}
